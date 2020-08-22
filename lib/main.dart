ee17b98359816ad9556b7ad94af8db2dc5032108
sahulhameed <sahulhameed.s@kredosystems.com>
2020-08-22 08:10:41 +0530

    background color app-bar and body

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Center(
             child: Text("I Am Rich"),
           ),
+          backgroundColor: Colors.blueGrey[900],
         ),
+        // body: Text("body"),
       ),
     ),
   );
